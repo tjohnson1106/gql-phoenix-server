a19a0e4938508b953d30fe2cb86f21af5e4c2e11
Thomas Johnson <tjohnson1106@gmail.com>
2018-06-08 12:31:21 -0400

    added schemna for posts users created DB

--- lib/graphical_web/router.ex
+++ lib/graphical_web/router.ex
@@ -7,5 +7,8 @@ defmodule GraphicalWeb.Router do
 
   scope "/api", GraphicalWeb do
     pipe_through :api
+    resources "/users", UserController, except: [:new, :edit]
+    resources "/posts", PostController, except: [:new, :edit]
+
   end
 end
